1efb3c5d535e6679c1957ac69e5e92d8be6cd10d
Felix Berger <felix.berger98@icloud.com>
2021-03-11 09:47:54 +0100

    DB-Access / Input-Validation
    - input user
    - get user
    - input validation
    - connection to db

---
+++ php/config.php
@@ -0,0 +1,13 @@
+<?php
+
+//Configuration details for database (on server)
+const DB_DSN_S = "mysql:host=localhost;port=3306;dbname=individumeal_comdb1";
+const DB_USER_S = "individumeal_comdb1";
+const DB_PASS_S = "getPasswordFromAdminPanel";
+
+//Configuration details for database (on localhost)
+const DB_DSN_L = "mysql:host=localhost;dbname=individumeal_comdb1";
+const DB_USER_L = "root";
+const DB_PASS_L = "";
+
+?>
