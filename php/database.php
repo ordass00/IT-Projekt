1e6012db3c77ff12213aec1abd928dd37070c48f
Felix Berger <felix.berger98@icloud.com>
2021-03-12 08:36:14 +0100

    Register User Website + Landingpage
    - Website for a user to register
    - Landingpage

--- php/database.php
+++ php/database.php
@@ -2,29 +2,29 @@
 include "config.php";
 include "input.php";
 
-function connect_server(){
-    try{
-      $conn = new PDO(DB_DSN_S, DB_USER_S, DB_PASS_S);
-      return $conn;
-    }
-    catch(PDOException $e){
-      echo $e->getMessage();
-    }
+function connect_server()
+{
+  try {
+    $conn = new PDO(DB_DSN_S, DB_USER_S, DB_PASS_S);
+    return $conn;
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+  }
 }
 
-function connect_local(){
-    try{
-      $conn = new PDO(DB_DSN_L, DB_USER_L, DB_PASS_L);
-      return $conn;
-    }
-    catch(PDOException $e){
-      echo $e->getMessage();
-    }
+function connect_local()
+{
+  try {
+    $conn = new PDO(DB_DSN_L, DB_USER_L, DB_PASS_L);
+    return $conn;
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+  }
 }
 
-function get_user_by_mail($conn, $email){
-  if($conn == null || $email == null){
-    echo "Input parameter(s) is/are missing";
+function get_user_by_mail($conn, $email)
+{
+  if ($conn == null || $email == null) {
     return null;
   }
   $email = validate_input($email);
@@ -37,9 +37,24 @@ function get_user_by_mail($conn, $email){
   return $result;
 }
 
-function set_user($conn, $firstName, $lastName, $dateOfBirth, $gender, $password, $email){
-  if($firstName == null || $lastName == null || $dateOfBirth == null || $gender == null || $password == null || $email == null){
-    echo "Input parameter(s) is/are missing";
+function get_user_by_username($conn, $username)
+{
+  if ($conn == null || $username == null) {
+    return null;
+  }
+  $username = validate_input($username);
+  //Preventing Sql-Injection with prepared statements
+  $sql = "select * from User where Username = ?;";
+  $stmt = $conn->prepare($sql);
+  $stmt->bindParam(1, $username);
+  $stmt->execute();
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+  return $result;
+}
+
+function set_user($conn, $firstName, $lastName, $dateOfBirth, $gender, $username, $password, $email)
+{
+  if ($firstName == null || $lastName == null || $dateOfBirth == null || $gender == null || $password == null || $email == null || $username == null) {
     return false;
   }
   $password = hash("sha256", $password);
@@ -48,12 +63,25 @@ function set_user($conn, $firstName, $lastName, $dateOfBirth, $gender, $password
   $dateOfBirth = validate_input($dateOfBirth);
   $gender = validate_input($gender);
   $email = validate_input($email);
+  $username = validate_input($username);
+
+  //Checking for duplicates
+  if (get_user_by_mail(connect_local(), $email)) {
+    return false;
+  }
+  if (get_user_by_username(connect_local(), $username)) {
+    return false;
+  }
+
   //Preventing Sql-Injection with prepared statements
-  $sql = "insert into User (FirstName, LastName, Gender, Password, EMail, DateOfBirth)
-  values (:firstName, :lastName, :gender, :password, :email, :dateOfBirth);";
+  $sql = "insert into User (FirstName, LastName, Username, Gender, Password, EMail, DateOfBirth)
+  values (:firstName, :lastName, :username, :gender, :password, :EMail, :dateOfBirth);";
   $stmt = $conn->prepare($sql);
-  $result = $stmt->execute(array(":firstName" => $firstName, ":lastName" => $lastName, ":gender" => $gender, ":password" => $password, "email" => $email, ":dateOfBirth" => $dateOfBirth));
-  if(!$result){
+  $result = $stmt->execute(array(
+    ":firstName" => $firstName, ":lastName" => $lastName, ":username" => $username, ":gender" => $gender,
+    ":password" => $password, ":EMail" => $email, ":dateOfBirth" => $dateOfBirth
+  ));
+  if (!$result) {
     return false;
   }
   return true;
