5e7d984e57c9204da10f053579c2d03198eb94b3
Oliver Dassinger <oliverdassinger@yahoo.de>
2021-04-08 21:26:46 +0200

    Refactored implementation of login functionality

--- login/login.ts
+++ login/login.ts
@@ -2,7 +2,7 @@ import {showToastMessage} from "../shared/js/shared_functions.js";
 
 export function successfullyRegisteredToast(): void {
     if (window.localStorage["registered"] == "true") {
-        showToastMessage("successfully_registered_toast")
+        showToastMessage("successfully_registered_toast");
         localStorage.clear();
     }
 }
