28d050af044bc55a91d41838562dc26c584130cf
Felix Berger <felix.berger98@icloud.com>
2021-04-21 23:27:22 +0200

    Password Reset Process

--- reset_password/reset_password.php
+++ reset_password/reset_password.php
@@ -8,13 +8,96 @@ header("Content-Type: application/json");
 $request = file_get_contents("php://input");
 
 if (isset($request) && !empty($request)) {
-    $reqObj = json_decode($request);
-    $email = get_user_by_mail($conn, $reqObj->email);
-    if (!$email) {
-        echo json_encode(["error" => true, "errorText" => "E-Mail not found", "resetToken" => ""]);
-    } else {
+  $reqObj = json_decode($request);
+  switch ($reqObj->method) {
+    case "reset_password_request":
+      $email = get_user_by_mail($conn, $reqObj->email);
+      if (!$email) {
+        echo json_encode(["error" => true, "errorText" => "E-Mail not found"]);
+      } else {
+        $id = $email["ID"];
+        $email = $email["EMail"];
+
         $random_token = random_bytes(64);
-        $expire_time = time() + (15*60);
-        echo json_encode(["error" => false, "errorText" => "", "resetToken" => $random_token]);
-    }
+        $random_token = password_hash($random_token, PASSWORD_DEFAULT);
+        $expire_time = time() + (15 * 60);
+        $password_reset_db = json_encode(["random_token" => $random_token, "expire_time" => $expire_time]);
+        $conn = connect_local();
+        $success = set_password_reset($conn, $id, $password_reset_db);
+        if (!$success) {
+          echo json_encode(["error" => true, "errorText" => "Failed to set reset-token"]);
+        }
+
+        $subject = "Reset password | individumeal.com";
+        $headers = "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=utf-8\r\n";
+        $headers .= "From: noreply@individumeal.com\r\n";
+        $message = ' 
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      
+        <title>Reset password</title>
+      </head>
+      
+      <body>
+        <div class="background_image" style="background-image: url(https://individumeal.com/shared/img/background.jpg); filter: blur(8px); -webkit-filter: blur(8px);
+        height: 100%; background-position: center; background-repeat: no-repeat; background-size: cover;"></div>
+        <main class="reset_password_form"
+          style="display: flex; justify-content: center; align-items: center; text-align: center;">
+          <form action="http://localhost/IT-Projekt/IT-Projekt/reset_password/set_new_password.php?token=' . $random_token . '&id=' . $id . '" style="background-color: rgb(0, 0, 0); background-color: rgba(152, 149, 158, 0.25); 
+          color: white; font-weight: bold; border-radius: 5px;position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 2; padding: 15px; 
+          margin: auto; text-align: center;">
+            <h1>Forgot your password?</h1>
+            <div class="reset_password_content">
+              <p>Just click the button below to reset it.</p>
+              <br>
+              <button type="submit">
+                Reset password
+              </button>
+            </div>
+            <p style="color: grey;">&copy; 2021 individumeal.com</p>
+          </form>
+        </main>
+      </body>
+      
+      </html>';
+        $success = mail($email, $subject, $message, $headers);
+        if ($success == $email) {
+          echo json_encode(["error" => false, "errorText" => ""]);
+        } else {
+          echo json_encode(["error" => true, "errorText" => "Failed to send email"]);
+        }
+      }
+      break;
+    case "reset_password":
+      $password = $reqObj->password;
+      $password = password_hash($password, PASSWORD_DEFAULT);
+      $conn = connect_local();
+
+      $password_reset_db = get_password_reset($conn, $reqObj->id);
+      $password_reset_dbObj = json_decode($password_reset_db["PasswordReset"]);
+      if ($password_reset_dbObj == null) {
+        echo json_encode(["error" => true, "errorText" => "Unable to load reset token/expiry time"]);
+        exit;
+      }
+      $token_db = $password_reset_dbObj->random_token;
+      $timestamp_db = $password_reset_dbObj->expire_time;
+
+      if ($token_db != $reqObj->token) {
+        echo json_encode(["error" => true, "errorText" => "Reset token did not match"]);
+        exit;
+      } else if ($timestamp_db < time()) {
+        echo json_encode(["error" => true, "errorText" => "The reset token was expired"]);
+        exit;
+      } else {
+        $success = reset_password($conn, $reqObj->id, $password);
+        clear_password_reset($conn, $reqObj->id);
+        if (!$success) {
+          echo json_encode(["error" => true, "errorText" => "Failed to reset password"]);
+        }
+        echo json_encode(["error" => false, "errorText" => ""]);
+      }
+      break;
+  }
 }
--- reset_password/reset_password.ts
+++ reset_password/reset_password.ts
@@ -3,7 +3,7 @@ export function request_password_reset(): boolean {
     let email = (document.getElementById("email_input") as HTMLInputElement).value;
 
     let reqObj = {
-        method: "reset_password",
+        method: "reset_password_request",
         email: email,
     };
     fetch("reset_password.php", {
@@ -16,7 +16,7 @@ export function request_password_reset(): boolean {
             }
             throw new Error("Error in response. (request_password_reset)");
         })
-        .then(function (data: { error: string; errorText: string; resetToken: string}) {
+        .then(function (data: { error: string; errorText: string; }) {
             if (data.error) {
                 showToastErrorMessage("error_toast", "error_text", data.errorText);
             } else {
@@ -39,4 +39,47 @@ function update_instructions(): void {
         showToastErrorMessage("error_toast", "error_text", "Please try again later!");
     }
 
+}
+export function set_new_password():boolean{
+    let id = document.getElementById("u_id")?.innerHTML;
+    let token = document.getElementById("token")?.innerHTML;
+    if(id == "" || token == ""){
+        showToastErrorMessage("error_toast", "error_text", "The process to reset the password was not started yet.");
+        return false;
+    }
+    let password = (document.getElementById("new_password") as HTMLInputElement).value;
+    let password_check = (document.getElementById("new_password_check") as HTMLInputElement).value;
+    if(password != password_check){
+        showToastErrorMessage("error_toast", "error_text", "The passwords did not match.");
+        return false;
+    }
+    let reqObj = {
+        method: "reset_password",
+        password: password,
+        token: token,
+        id: id
+    };
+    fetch("reset_password.php", {
+        method: "POST",
+        body: JSON.stringify(reqObj),
+    })
+        .then(function (response: Response) {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error("Error in response. (set_new_password)");
+        })
+        .then(function (data: { error: string; errorText: string; }) {
+            if (data.error) {
+                showToastErrorMessage("error_toast", "error_text", data.errorText);
+            } else {
+                localStorage.setItem('password_reset', 'success');
+                window.location.href = "../login/login_form.php";
+            }
+        })
+    ["catch"](function (error) {
+        showToastErrorMessage("error_toast", "error_text", error.errorText);
+    });
+
+    return false;
 }
