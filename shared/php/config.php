2eb07d5fc6653cfbd1e21a0763ff0430fe5bbc04
Oliver Dassinger <oliverdassinger@yahoo.de>
2021-05-09 12:08:35 +0200

    changed redirection webside

--- shared/php/config.php
+++ shared/php/config.php
@@ -6,7 +6,7 @@ const DB_USER_S = "individumeal_comdb1";
 const DB_PASS_S = "getPasswordFromAdminPanel";
 
 //Configuration details for database (on localhost)
-const DB_DSN_L = "mysql:host=localhost;port=3307;dbname=individumeal_comdb1";
+const DB_DSN_L = "mysql:host=localhost;dbname=individumeal_comdb1";
 const DB_USER_L = "root";
 const DB_PASS_L = "";
 
