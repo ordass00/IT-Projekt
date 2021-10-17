7fc56330b25e8d77f922b85e8b63b4cf987dc0f5
Oliver Dassinger <oliverdassinger@yahoo.de>
2021-10-17 21:19:08 +0200

    Password validation + bugfixes
    - Added password validation to register personal account details and
      password reset so its uniform with password validation from change
      account settings.

--- shared/js/shared_functions.js
+++ shared/js/shared_functions.js
@@ -21,3 +21,82 @@ export function showToastMessage(id) {
     let toastElement = new bootstrap.Toast(toastHTMLElement);
     toastElement.show();
 }
+export function show_password_validation(password_input_field_or_password_repeat_input_field) {
+    if(password_input_field_or_password_repeat_input_field == "password_input_field"){
+        document.getElementsByClassName("pwd-validation")[0].style.visibility = "visible";
+    } else {
+        document.getElementsByClassName("pwd-validation")[1].style.visibility = "visible";
+    }
+}
+export function hide_password_validation(password_input_field_or_password_repeat_input_field){
+    if(password_input_field_or_password_repeat_input_field == "password_input_field"){
+        document.getElementsByClassName("pwd-validation")[0].style.visibility = "hidden";
+    } else {
+        document.getElementsByClassName("pwd-validation")[1].style.visibility = "hidden";
+    }
+}
+export function validate_password(input_id){
+    let pwd_input = document.getElementById(input_id);
+    let lower_case_letters_error_msg = document.getElementById("lower_case");
+    let upper_case_letters_error_msg = document.getElementById("upper_case");
+    let numbers_error_msg = document.getElementById("one_number");
+    let min_eight_chars_error_msg = document.getElementById("min_eight_chars");
+    let lower_case_letters = /[a-z]/g;
+    let upper_case_letters = /[A-Z]/g;
+    let numbers = /[0-9]/g;
+    if (pwd_input.value.match(lower_case_letters)) {
+        lower_case_letters_error_msg.classList.remove("bi-x-lg");
+        lower_case_letters_error_msg.classList.add("bi-check-lg");
+        lower_case_letters_error_msg.style["color"] = "green";
+    }
+    else {
+        lower_case_letters_error_msg.classList.remove("bi-check-lg");
+        lower_case_letters_error_msg.classList.add("bi-x-lg");
+        lower_case_letters_error_msg.style["color"] = "red";
+    }
+    if (pwd_input.value.match(upper_case_letters)) {
+        upper_case_letters_error_msg.classList.remove("bi-x-lg");
+        upper_case_letters_error_msg.classList.add("bi-check-lg");
+        upper_case_letters_error_msg.style["color"] = "green";
+    }
+    else {
+        upper_case_letters_error_msg.classList.remove("bi-check-lg");
+        upper_case_letters_error_msg.classList.add("bi-x-lg");
+        upper_case_letters_error_msg.style["color"] = "red";
+    }
+    if (pwd_input.value.match(numbers)) {
+        numbers_error_msg.classList.remove("bi-x-lg");
+        numbers_error_msg.classList.add("bi-check-lg");
+        numbers_error_msg.style["color"] = "green";
+    }
+    else {
+        numbers_error_msg.classList.remove("bi-check-lg");
+        numbers_error_msg.classList.add("bi-x-lg");
+        numbers_error_msg.style["color"] = "red";
+    }
+    if (pwd_input.value.length >= 8) {
+        min_eight_chars_error_msg.classList.remove("bi-x-lg");
+        min_eight_chars_error_msg.classList.add("bi-check-lg");
+        min_eight_chars_error_msg.style["color"] = "green";
+    }
+    else {
+        min_eight_chars_error_msg.classList.remove("bi-check-lg");
+        min_eight_chars_error_msg.classList.add("bi-x-lg");
+        min_eight_chars_error_msg.style["color"] = "red";
+    }
+}
+export function validate_password_repeat(input_id, repeat_input_id) {
+    let pwd_input = document.getElementById(input_id);
+    let repeat_pwd_input = document.getElementById(repeat_input_id);
+    let pwd_match_error_msg = document.getElementById("password_match");
+    if (repeat_pwd_input.value != pwd_input.value) {
+        pwd_match_error_msg.classList.remove("bi-check-lg");
+        pwd_match_error_msg.classList.add("bi-x-lg");
+        pwd_match_error_msg.style["color"] = "red";
+    }
+    else {
+        pwd_match_error_msg.classList.remove("bi-x-lg");
+        pwd_match_error_msg.classList.add("bi-check-lg");
+        pwd_match_error_msg.style["color"] = "green";
+    }
+}
